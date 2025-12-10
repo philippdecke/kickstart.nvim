4f06263711ac3aca2e0b4e5f596391b3ffd40f4a
Philipp Decke <101802850+philippdecke@users.noreply.github.com>
2025-12-10 15:56:39 +0100

    add vim-slime plugin for python code chunk execution

--- init.lua
+++ init.lua
@@ -302,6 +302,36 @@ require('lazy').setup({
     },
   },
 
+  {
+    'jpalardy/vim-slime',
+    init = function()
+      vim.g.slime_target = 'neovim'
+      vim.g.slime_python_ipython = 1
+      vim.g.slime_dispatch_ipython_pause = 100
+      vim.g.slime_cell_delimiter = '#\\s\\=%%'
+      vim.cmd [[
+      function! _EscapeText_quarto(text)
+        if slime#config#resolve("python_ipython") && len(split(a:text,"\n")) > 1
+          return ["%cpaste -q\n", slime#config#resolve("dispatch_ipython_pause"), a:text, "--\n"]
+      else
+        let empty_lines_pat = '\(^\|\n\)\zs\(\s*\n\+\)\+'
+        let no_empty_lines = substitute(a:text, empty_lines_pat, "", "g")
+        let dedent_pat = '\(^\|\n\)\zs'.matchstr(no_empty_lines, '^\s*')
+        let dedented_lines = substitute(no_empty_lines, dedent_pat, "", "g")
+        let except_pat = '\(elif\|else\|except\|finally\)\@!'
+        let add_eol_pat = '\n\s[^\n]\+\n\zs\ze\('.except_pat.'\S\|$\)'
+        return substitute(dedented_lines, add_eol_pat, "\n", "g")
+      end
+      endfunction
+      ]]
+    end,
+    config = function()
+      vim.keymap.set({ 'n', 'i' }, '<leader>sc', function()
+        vim.cmd [[ call slime#send_cell() ]]
+      end, { desc = '[s]end [c]ode to terminal' })
+    end,
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
